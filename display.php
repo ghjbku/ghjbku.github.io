e557c09fbb967ca404ebebb3257658fb4116050a
Balázs Tóth <gawnoris@gmail.com>
2022-01-03 21:26:27 +0100

    Update display.php

--- display.php
+++ display.php
@@ -9,17 +9,5 @@ $con=mysqli_connect($servername,$username,$password);
 mysqli_select_db($dbname,$con);
 $result=mysqli_query("select * from DataTable",$con);
 
-echo "<table border='1' >
-<tr>
-<td align=center> <b>ID</b></td>
-<td align=center><b>DATA</b></td>
-
-while($data = mysqli_fetch_row($result))
-{   
-    echo "<tr>";
-    echo "<td align=center>$data[0]</td>";
-    echo "<td align=center>$data[1]</td>";
-    echo "</tr>";
-}
-echo "</table>";
+echo $result;
 ?>
